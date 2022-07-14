38a45a253371991215112e27ba551ef3118607c9
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-07-14 03:04:08 +0200

    wip

--- src/components/CommandBar.tsx
+++ src/components/CommandBar.tsx
@@ -1,20 +1,20 @@
-// import { useTheme } from 'next-themes'
+import { useTheme } from 'next-themes'
 // import { toast } from 'react-hot-toast'
 // import { useRouter } from 'next/router'
-import React, { FC, forwardRef, PropsWithChildren, PropsWithRef, Ref, useMemo, useRef } from 'react'
-// import {
-// 	CodeIcon,
-// 	MoonIcon,
-// 	DuplicateIcon,
-// 	HomeIcon,
-// 	MailIcon,
-// 	SunIcon,
-// 	UserIcon,
-// 	DesktopComputerIcon,
-// 	RssIcon,
-// 	DocumentTextIcon,
-// 	CollectionIcon,
-// } from '@heroicons/react/outline'
+import React, { FC, forwardRef, Fragment, PropsWithChildren, PropsWithRef, Ref, useMemo, useRef } from 'react'
+import {
+	// 	CodeIcon,
+	MoonIcon,
+	// 	DuplicateIcon,
+	// 	HomeIcon,
+	// 	MailIcon,
+	SunIcon,
+	// 	UserIcon,
+	DesktopComputerIcon,
+	// 	RssIcon,
+	// 	DocumentTextIcon,
+	// 	CollectionIcon,
+} from '@heroicons/react/outline'
 import {
 	ActionId,
 	ActionImpl,
@@ -30,159 +30,158 @@ import {
 	useMatches,
 } from 'kbar'
 
-// enum Sections {
-// 	Navigation = 'navigation',
-// 	Socials = 'socials',
-// 	General = 'general',
-// }
+enum Sections {
+	Navigation = 'navigation',
+	Socials = 'socials',
+	General = 'general',
+}
 
 const CommandBar: FC<PropsWithChildren<{}>> = ({ children }) => {
 	// const router = useRouter()
-	// const { setTheme } = useTheme()
+	const { setTheme } = useTheme()
 
-	// const actions = [
-	// 	{
-	// 		id: 'home',
-	// 		name: 'Home',
-	// 		icon: <HomeIcon />,
-	// 		shortcut: ['h'],
-	// 		section: Sections.Navigation,
-	// 		keywords: 'home back main root index',
-	// 		perform: () => router.push('/'),
-	// 	},
-	// 	{
-	// 		id: 'about',
-	// 		name: 'About',
-	// 		icon: <UserIcon />,
-	// 		shortcut: ['a'],
-	// 		section: Sections.Navigation,
-	// 		keywords: 'info',
-	// 		perform: () => router.push('/about'),
-	// 	},
-	// 	{
-	// 		id: 'blog',
-	// 		name: 'Blog',
-	// 		icon: <DocumentTextIcon />,
-	// 		shortcut: ['b'],
-	// 		section: Sections.Navigation,
-	// 		keywords: 'writing articles content',
-	// 		perform: () => router.push('/blog'),
-	// 	},
-	// 	{
-	// 		id: 'stack',
-	// 		name: 'Stack',
-	// 		icon: <CollectionIcon />,
-	// 		shortcut: ['s'],
-	// 		section: Sections.Navigation,
-	// 		keywords: 'gear tools',
-	// 		perform: () => router.push('/stack'),
-	// 	},
-	// 	{
-	// 		id: 'words',
-	// 		name: 'Words',
-	// 		icon: <DocumentTextIcon />,
-	// 		shortcut: ['w'],
-	// 		section: Sections.Navigation,
-	// 		keywords: 'vocables',
-	// 		perform: () => router.push('/words'),
-	// 	},
-	// 	// {
-	// 	// 	id: 'twitter',
-	// 	// 	name: 'Twitter',
-	// 	// 	icon: <TwitterIcon />,
-	// 	// 	section: Sections.Socials,
-	// 	// 	keywords: 'twitter tweet',
-	// 	// 	perform: () => window.open('https://twitter.com/lavgup'),
-	// 	// },
-	// 	// {
-	// 	// 	id: 'github',
-	// 	// 	name: 'GitHub',
-	// 	// 	icon: <GitHubIcon />,
-	// 	// 	shortcut: ['g'],
-	// 	// 	section: Sections.Socials,
-	// 	// 	perform: () => window.open('https://github.com/lavgup'),
-	// 	// },
-	// 	{
-	// 		id: 'mail',
-	// 		name: 'Mail',
-	// 		icon: <MailIcon />,
-	// 		shortcut: ['m'],
-	// 		section: Sections.Socials,
-	// 		keywords: 'email contact',
-	// 		perform: () => window.open('mailto://lavyag01@gmail.com'),
-	// 	},
-	// 	{
-	// 		id: 'theme',
-	// 		name: 'Change theme...',
-	// 		shortcut: ['t'],
-	// 		icon: <DesktopComputerIcon />,
-	// 		section: Sections.General,
-	// 	},
-	// 	{
-	// 		id: 'light',
-	// 		name: 'Light',
-	// 		icon: <SunIcon />,
-	// 		section: '',
-	// 		keywords: 'light theme',
-	// 		parent: 'theme',
-	// 		perform: () => setTheme('light'),
-	// 	},
-	// 	{
-	// 		id: 'dark',
-	// 		name: 'Dark',
-	// 		icon: <MoonIcon />,
-	// 		section: '',
-	// 		keywords: 'dark theme',
-	// 		parent: 'theme',
-	// 		perform: () => setTheme('dark'),
-	// 	},
-	// 	{
-	// 		id: 'system',
-	// 		name: 'System',
-	// 		icon: <DesktopComputerIcon />,
-	// 		section: '',
-	// 		keywords: 'system theme',
-	// 		parent: 'theme',
-	// 		perform: () => setTheme('system'),
-	// 	},
-	// 	{
-	// 		id: 'copy-url',
-	// 		name: 'Copy URL to clipboard',
-	// 		icon: <DuplicateIcon />,
-	// 		shortcut: ['cc'],
-	// 		section: Sections.General,
-	// 		keywords: 'copy share url',
-	// 		perform: async () => {
-	// 			await navigator.clipboard.writeText('https://lavya.me' + router.asPath)
+	const actions = [
+		// 	{
+		// 		id: 'home',
+		// 		name: 'Home',
+		// 		icon: <HomeIcon />,
+		// 		shortcut: ['h'],
+		// 		section: Sections.Navigation,
+		// 		keywords: 'home back main root index',
+		// 		perform: () => router.push('/'),
+		// 	},
+		// 	{
+		// 		id: 'about',
+		// 		name: 'About',
+		// 		icon: <UserIcon />,
+		// 		shortcut: ['a'],
+		// 		section: Sections.Navigation,
+		// 		keywords: 'info',
+		// 		perform: () => router.push('/about'),
+		// 	},
+		// 	{
+		// 		id: 'blog',
+		// 		name: 'Blog',
+		// 		icon: <DocumentTextIcon />,
+		// 		shortcut: ['b'],
+		// 		section: Sections.Navigation,
+		// 		keywords: 'writing articles content',
+		// 		perform: () => router.push('/blog'),
+		// 	},
+		// 	{
+		// 		id: 'stack',
+		// 		name: 'Stack',
+		// 		icon: <CollectionIcon />,
+		// 		shortcut: ['s'],
+		// 		section: Sections.Navigation,
+		// 		keywords: 'gear tools',
+		// 		perform: () => router.push('/stack'),
+		// 	},
+		// 	{
+		// 		id: 'words',
+		// 		name: 'Words',
+		// 		icon: <DocumentTextIcon />,
+		// 		shortcut: ['w'],
+		// 		section: Sections.Navigation,
+		// 		keywords: 'vocables',
+		// 		perform: () => router.push('/words'),
+		// 	},
+		// 	// {
+		// 	// 	id: 'twitter',
+		// 	// 	name: 'Twitter',
+		// 	// 	icon: <TwitterIcon />,
+		// 	// 	section: Sections.Socials,
+		// 	// 	keywords: 'twitter tweet',
+		// 	// 	perform: () => window.open('https://twitter.com/lavgup'),
+		// 	// },
+		// 	// {
+		// 	// 	id: 'github',
+		// 	// 	name: 'GitHub',
+		// 	// 	icon: <GitHubIcon />,
+		// 	// 	shortcut: ['g'],
+		// 	// 	section: Sections.Socials,
+		// 	// 	perform: () => window.open('https://github.com/lavgup'),
+		// 	// },
+		// 	{
+		// 		id: 'mail',
+		// 		name: 'Mail',
+		// 		icon: <MailIcon />,
+		// 		shortcut: ['m'],
+		// 		section: Sections.Socials,
+		// 		keywords: 'email contact',
+		// 		perform: () => window.open('mailto://lavyag01@gmail.com'),
+		// 	},
+		{
+			id: 'theme',
+			name: 'Change theme...',
+			shortcut: ['t'],
+			icon: <DesktopComputerIcon />,
+			section: Sections.General,
+		},
+		{
+			id: 'light',
+			name: 'Light',
+			icon: <SunIcon />,
+			section: '',
+			keywords: 'light theme',
+			parent: 'theme',
+			perform: () => setTheme('light'),
+		},
+		{
+			id: 'dark',
+			name: 'Dark',
+			icon: <MoonIcon />,
+			section: '',
+			keywords: 'dark theme',
+			parent: 'theme',
+			perform: () => setTheme('dark'),
+		},
+		{
+			id: 'system',
+			name: 'System',
+			icon: <DesktopComputerIcon />,
+			section: '',
+			keywords: 'system theme',
+			parent: 'theme',
+			perform: () => setTheme('system'),
+		},
+		// 	{
+		// 		id: 'copy-url',
+		// 		name: 'Copy URL to clipboard',
+		// 		icon: <DuplicateIcon />,
+		// 		shortcut: ['cc'],
+		// 		section: Sections.General,
+		// 		keywords: 'copy share url',
+		// 		perform: async () => {
+		// 			await navigator.clipboard.writeText('https://lavya.me' + router.asPath)
 
-	// 			toast.success('Copied to clipboard!', {
-	// 				className: 'text-neutral-600 dark:text-neutral-200/[.85] bg-white dark:bg-neutral-800',
-	// 				duration: 3000,
-	// 			})
-	// 		},
-	// 	},
-	// 	{
-	// 		id: 'rss',
-	// 		name: 'RSS Feed',
-	// 		icon: <RssIcon />,
-	// 		section: Sections.General,
-	// 		keywords: 'rss feed atom',
-	// 		perform: async () => {
-	// 			await navigator.clipboard.writeText('https://lavya.me/feed.xml')
-	// 			await router.push('https://lavya.me/feed.xml')
-	// 		},
-	// 	},
-	// 	{
-	// 		id: 'source',
-	// 		name: 'View source',
-	// 		icon: <CodeIcon />,
-	// 		shortcut: ['sc'],
-	// 		section: Sections.General,
-	// 		keywords: 'source code',
-	// 		perform: () => window.open('https://github.com/lavgup/lavya.me'),
-	// 	},
-	// ]
-	const actions = []
+		// 			toast.success('Copied to clipboard!', {
+		// 				className: 'text-neutral-600 dark:text-neutral-200/[.85] bg-white dark:bg-neutral-800',
+		// 				duration: 3000,
+		// 			})
+		// 		},
+		// 	},
+		// 	{
+		// 		id: 'rss',
+		// 		name: 'RSS Feed',
+		// 		icon: <RssIcon />,
+		// 		section: Sections.General,
+		// 		keywords: 'rss feed atom',
+		// 		perform: async () => {
+		// 			await navigator.clipboard.writeText('https://lavya.me/feed.xml')
+		// 			await router.push('https://lavya.me/feed.xml')
+		// 		},
+		// 	},
+		// 	{
+		// 		id: 'source',
+		// 		name: 'View source',
+		// 		icon: <CodeIcon />,
+		// 		shortcut: ['sc'],
+		// 		section: Sections.General,
+		// 		keywords: 'source code',
+		// 		perform: () => window.open('https://github.com/lavgup/lavya.me'),
+		// 	},
+	]
 
 	return (
 		<KBarProvider actions={actions} options={{ enableHistory: true }}>
@@ -234,10 +233,9 @@ const Breadcrumb: FC<{ text: string; query: KBarQuery }> = ({ text, query }) => 
 
 const Results = () => {
 	const { results, rootActionId } = useMatches()
-	const wrapperRef = useRef(null)
 
 	return (
-		<div className="relative" ref={wrapperRef}>
+		<div className="relative">
 			<KBarResults
 				items={results.filter(i => i !== NO_GROUP)}
 				onRender={({ item, active }) =>
@@ -259,8 +257,9 @@ const ResultItem: FC<
 		currentRootActionId: ActionId
 	}>
 > = forwardRef(({ action, active, currentRootActionId }, ref: Ref<HTMLDivElement>) => {
-	const ancestors = React.useMemo(() => {
+	const ancestors = useMemo(() => {
 		if (!currentRootActionId) return action.ancestors
+
 		const index = action.ancestors.findIndex(ancestor => ancestor.id === currentRootActionId)
 		return action.ancestors.slice(index + 1)
 	}, [action.ancestors, currentRootActionId])
@@ -283,10 +282,10 @@ const ResultItem: FC<
 					<div>
 						{ancestors.length > 0 &&
 							ancestors.map((ancestor: ActionImpl) => (
-								<React.Fragment key={ancestor.id}>
+								<Fragment key={ancestor.id}>
 									<span>{ancestor.name}</span>
 									<span className="mr-0.5">&rsaquo;</span>
-								</React.Fragment>
+								</Fragment>
 							))}
 						<span>{action.name}</span>
 					</div>
