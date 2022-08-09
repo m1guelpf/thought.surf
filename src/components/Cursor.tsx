30780d5ae64e89424c029712a16f1da9074a0a35
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-08-08 19:43:42 -0700

    wip

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -3,6 +3,7 @@ import { Point } from '@/types/canvas'
 import { FC, memo, useMemo } from 'react'
 import { canvasToScreen } from '@/lib/canvas'
 import { useRefCamera } from '@/store/camera'
+import * as Avatar from '@radix-ui/react-avatar'
 
 type Props = { pos: Point; color: string; name?: string; avatar?: string; emoji: string }
 
@@ -22,22 +23,23 @@ const Cursor: FC<Props> = ({ pos, color, name, avatar, emoji }) => {
 			className="absolute pointer-events-none top-0 left-0 w-14 h-14 z-20 overflow-hidden border-4 border-white dark:border-gray-900 rounded-full rounded-tl-none flex items-center justify-center"
 			transition={{ type: 'spring', damping: 30, mass: 0.8, stiffness: 350 }}
 		>
-			{avatar ? (
-				<img src={avatar} alt={name} className="w-full h-full" />
-			) : (
-				<p
-					className={`leading-relaxed text-white ${
-						name ? 'text-lg' : 'text-2xl'
-					} whitespace-nowrap uppercase`}
-				>
-					{name
-						? name
-								.split(' ')
-								.map(word => word[0])
-								.join('')
-						: emoji}
-				</p>
-			)}
+			<Avatar.Root>
+				<Avatar.Image src={avatar} alt={name} className="w-full h-full" />
+				<Avatar.Fallback>
+					<p
+						className={`leading-relaxed text-white ${
+							name ? 'text-lg' : 'text-2xl'
+						} whitespace-nowrap uppercase`}
+					>
+						{name
+							? name
+									.split(' ')
+									.map(word => word[0])
+									.join('')
+							: emoji}
+					</p>
+				</Avatar.Fallback>
+			</Avatar.Root>
 		</motion.div>
 	)
 }
