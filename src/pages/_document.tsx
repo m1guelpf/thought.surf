d2d13ac5222a88f4bf9d9e5bdd36aa430f1b6ba7
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-08-27 22:40:04 +0200

    wip

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
 	return (
-		<Html className="h-full bg-gray-100">
+		<Html className="h-full">
 			<Head />
-			<body className="h-full">
+			<body className="h-full bg-gray-100 dark:bg-black">
 				<Main />
 				<NextScript />
 			</body>
